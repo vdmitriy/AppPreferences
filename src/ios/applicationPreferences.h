a53eb3a3b54c6683d26010996bf9b21735877936
Dmitriy Voeykov <dmitriy.voeykov@yandex.ru>
2014-07-18 18:14:16 +0400

    Update applicationPreferences.h

--- src/ios/applicationPreferences.h
+++ src/ios/applicationPreferences.h
@@ -1,16 +1,17 @@
 //
 //  applicationPreferences.h
-//  
+//
 //
 //  Created by Tue Topholm on 31/01/11.
 //  Copyright 2011 Sugee. All rights reserved.
 //
+// Updated for Phonegap 3.x by Dave Alden (dave@workingedge.co.uk) on 05 Oct 13
 
 #import <Foundation/Foundation.h>
 
 #import <Cordova/CDVPlugin.h>
 
-@interface applicationPreferences : CDVPlugin 
+@interface applicationPreferences : CDVPlugin
 {
 
 }
